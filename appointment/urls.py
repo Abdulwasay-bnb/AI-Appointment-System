c897249b79980a7c411356bdfc0f047dca712930
Abdulwasay <164003533+Abdulwasay-bnb@users.noreply.github.com>
2025-06-21 00:46:40 +0500

    Added missing url

--- appointment/urls.py
+++ appointment/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     path('events/', views.event_list, name='event_list'),
     path('event/create/', views.event_create, name='event_create'),
     path('event/<uuid:event_id>/', views.event_detail, name='event_detail'),
+    path('event/<uuid:event_id>/update/', views.event_update, name='event_update'),
     path('event/<uuid:event_id>/edit/', views.event_update, name='event_update'),
     path('event/<uuid:event_id>/delete/', views.event_delete, name='event_delete'),
     
